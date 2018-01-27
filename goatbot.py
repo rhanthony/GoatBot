cfacc52004543cce2f29baba0d0ce0266be95e5e
rhanthony <rhanthony@dashin.net>
2018-01-27 22:59:24 +0000

    Messing with irc.py to try and get a working connection

--- goatbot.py
+++ goatbot.py
@@ -2,9 +2,9 @@ from irc import *
 import os
 import random
 
-channel = "#DnetTest"
+channel = "#Dashin"
 server = "losangeles.ca.us.undernet.org"
-nickname = "DnetPY"
+nickname = "ElecMoBot"
 
 irc = IRC()
 irc.connect(server, channel, nickname)
@@ -13,6 +13,10 @@ while 1:
 	text = irc.get_text()
 	print text
 
+	if 'PING' in text != -1:
+		irc.send('PONG ' + text.split() [1] + 'rn')
+
 	if "PRIVMSG" in text and channel in text and "snakes" in text:
-		irc.send(channel, "Hisssssssss")
+		hiss = "Hi" + random.randint(7,14)
+		irc.send(channel, hiss)
 
--- irc.py
+++ irc.py
@@ -9,25 +9,31 @@ class IRC:
         def __init__(self):
                 self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        def send(self, chan, msg):
-                self.irc.send("PRIVMSG " + chan + " " + msg + "n")
+        def send(self, target, msg):
+                self.irc.send("PRIVMSG " + target + " " + msg + "n")
 
         def connect(self, server, channel, botnick):
                 # defines the socket
                 print "connecting to:"+server
                 self.irc.connect((server, 6667))
-		self.irc.send("USER " + botnick + " * 8 :OatTheGoat" + "n")
-		self.irc.send("NICK " + botnick + "n")
-                self.irc.send("JOIN " + channel + "n")
+		print "CONNECTED!"
+		time.sleep(5)
 
+		print "Sending USER information..."
+		self.irc.send("USER " + botnick + " * 8 :OatTheGoat" + "\r\n")
+
+		#print("Sending NICK change to " + botnick)
+		#self.irc.send("NICK " + botnick)
+
+		#print("Sending JOIN command for " + channel)
+                #self.irc.send("JOIN " + channel)
 
         def get_text(self):
                 # receive text from socket
                 text = self.irc.recv(512)
 
                 # respond to all pings
-                if 'PING' in text != -1:
-			print text
+                if 'PING ' in text != -1:
                         self.irc.send('PONG ' + text.split() [1] + 'rn')
 
                 return text
