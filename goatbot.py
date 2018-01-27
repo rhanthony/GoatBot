8eb36c58e62ff50b2960e5d95f96459994867a3d
root <root@palm.dashin.net>
2018-01-27 19:29:54 +0000

    Initial commit of irc.py and goatbot.py

---
+++ goatbot.py
@@ -0,0 +1,20 @@
+from irc import *
+import os
+import random
+
+channel = "#DnetTest"
+server = "losangeles.ca.us.undernet.org"
+nickname = "DnetPY"
+
+irc = IRC()
+irc.connect(server, channel, nickname)
+
+while 1:
+	text = irc.get_text()
+	print text
+
+	if "PRIVMSG" in text and channel in text and "snakes" in text:
+		irc.send(channel, "Hisssssssss")
+
+	if "PRIVMSG" in text and channel in text and "!opdad" in text:
+		irc.send(channel, "/mode +o DnetDad")
---
+++ irc.py
@@ -0,0 +1,34 @@
+import socket
+import time
+import sys
+
+class IRC:
+
+        irc = socket.socket()
+
+        def __init__(self):
+                self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        def send(self, chan, msg):
+                self.irc.send("PRIVMSG " + chan + " " + msg + "n")
+
+        def connect(self, server, channel, botnick):
+                # defines the socket
+                print "connecting to:"+server
+                self.irc.connect((server, 6667))
+		self.irc.send("USER " + botnick + " * 8 :my IRC bot v0.1" + "n")
+		self.irc.send("NICK " + botnick + "n")
+                self.irc.send("JOIN " + channel + "n")
+
+
+        def get_text(self):
+                # receive text from socket
+                text = self.irc.recv(2040)
+
+                # respond to all pings
+                if 'PING' in text != -1:
+			print text
+                        self.irc.send('PONG ' + text.split() [1] + 'rn')
+
+                return text
+
